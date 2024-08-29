e42174c838b8668191c1bfdadfafd8217c3a7155
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-08-29 12:05:36 +0300

    Initial commit

---
+++ suppliers/app/admin.py
@@ -0,0 +1,13 @@
+# Register your models here.
+
+from django.contrib import admin
+
+from app.models import Supplier, Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    pass
